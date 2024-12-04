e6e18cf4746f91e16bddad0584178ed6697a7a24
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2024-12-04 09:16:55 +0700

    feat: add password hashing and comparison for admin model

--- app/api/v1/admin/model.js
+++ app/api/v1/admin/model.js
@@ -1,4 +1,5 @@
 const { model, Schema } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = Schema({
 	name: {
@@ -7,12 +8,26 @@ const adminSchema = Schema({
 	},
 	email: {
 		type: String,
-		required: true
+		required: true,
+		unique: true
 	},
 	password: {
 		type: String,
 		required: true
 	}
-})
+});
+
+adminSchema.pre('save', async function(next) {
+	const Admin = this;
+	if (Admin.isModified('password')) {
+		Admin.password = await bcrypt.hash(Admin.password, 12);
+	}
+	next();
+});
+
+adminSchema.methods.comparePassword = async function(candidatePassword) {
+	const isMatch = await bcrypt.compare(candidatePassword, this.password);
+	return isMatch;
+};
 
 module.exports = model('Admin', adminSchema);
--- app/services/mongoose/admin.js
+++ app/services/mongoose/admin.js
@@ -1,9 +1,16 @@
 const Admin = require('../../api/v1/admin/model');
+const { BadRequestError } = require('../../errors');
 
 const createAdmin = async (req) => {
-	const { name, email, password } = req.body;
+	const { name, email, password, confirmPassword } = req.body;
+
+	if (password !== confirmPassword) {
+		throw new BadRequestError('Password dan Konfirmasi Password tidak cocok');
+	}
 
 	const result = await Admin.create({ name, email, password });
+
+	delete result._doc.password;
 
 	return result;
 }
