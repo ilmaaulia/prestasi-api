bce91a25ac099c650be75e7da9108d27f1b260e5
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2024-12-24 08:47:34 +0700

    feat: add unauthorized and unauthenticated error class

--- app/errors/index.js
+++ app/errors/index.js
@@ -1,9 +1,13 @@
 const CustomApiError = require('./custom-api-error');
 const BadRequestError = require('./bad-request');
 const NotFoundError = require('./not-found');
+const UnauthorizedError = require('./unauthorized');
+const unauthenticatedError = require('./unauthenticated');
 
 module.exports = {
 	CustomApiError,
 	BadRequestError,
 	NotFoundError,
+	UnauthorizedError,
+	unauthenticatedError
 }
