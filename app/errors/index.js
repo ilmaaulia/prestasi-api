43e92fd57271c95ab7e9631889d7fd11017e1df9
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2024-12-24 11:05:56 +0700

    fix: correct variable naming for unauthenticated error export

--- app/errors/index.js
+++ app/errors/index.js
@@ -2,12 +2,12 @@ const CustomApiError = require('./custom-api-error');
 const BadRequestError = require('./bad-request');
 const NotFoundError = require('./not-found');
 const UnauthorizedError = require('./unauthorized');
-const unauthenticatedError = require('./unauthenticated');
+const UnauthenticatedError = require('./unauthenticated');
 
 module.exports = {
 	CustomApiError,
 	BadRequestError,
 	NotFoundError,
 	UnauthorizedError,
-	unauthenticatedError
+	UnauthenticatedError
 }
