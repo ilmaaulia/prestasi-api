360922dd47228532d64850484a9799891db53312
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-01-05 05:15:52 +0700

    feat: add otp email service

---
+++ app/services/email/index.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+const Mustache = require('mustache');
+const { senderEmail, password } = require('../../config');
+const fs = require('fs');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: senderEmail,
+    pass: password,
+  },
+});
+
+const otpMail = async (email, data) => {
+  try {
+    let template = fs.readFileSync('app/views/email/otp.html', 'utf8');
+
+    let message = {
+      from: '"noreply" <ilmaaulia@gmail.com>',
+      to: email,
+      subject: 'One Time Password',
+      html: Mustache.render(template, data),
+    };
+
+    return await transporter.sendMail(message);
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+module.exports = { otpMail };
