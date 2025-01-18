4685d3a993cd5ffc4e40f566f777cc1b7b8215d8
Ilma Aulia <87630364+ilmaaulia@users.noreply.github.com>
2025-01-18 08:00:35 +0700

    chore: database uri prod

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  urlDb : process.env.URL_MONGODB_DEV,
+  urlDb : process.env.URL_MONGODB_PROD,
   jwtExpiration: '2h',
   jwtSecret: process.env.JWT_SECRET,
   senderEmail: process.env.EMAIL,
